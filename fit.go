e0b24daf6beddc29f0296bb3222378719cd07f28
Phil Mansfield <mansfield@uchicago.edu>
2016-11-24 18:49:59 -0600

    API is mostly written out

--- fit.go
+++ fit.go
@@ -15,15 +15,34 @@ type Func func(x float64, param []float64) (y float64)
 func Curve(f Func) Model { return nil }
 
 type Model interface {
-	UnknownErrors(p []Parameter, x, y []float64) (Output, error)
+	UnknownErrors(p0 []Parameter, x, y []float64) (Output, error)
 
-	YErrors(p []Parameter, x, y, yerr []float64) (Output, error)
-	XErrors(p []Parameter, x, y, xerr []float64) (Output, error)
- 	XYErrors(p []Parameter, x, y, xerr, yerr []float64) (Output, error)
+	YErrors(p0 []Parameter, x, y, yerr []float64) (Output, error)
+	XErrors(p0 []Parameter, x, y, xerr []float64) (Output, error)
+ 	XYErrors(p0 []Parameter, x, y, xerr, yerr []float64) (Output, error)
 
-	YErrorsAndScatter(p []Parameter, x, y, yerr []float64) (Output, error)
-	XErrorsAndScatter(p []Parameter, x, y, xerr []float64) (Output, error)
- 	XYErrorsAndScatter(p []Parameter, x, y, xerr, yerr []float64) (Output, error)
+	YErrorsAndScatter(p0 []Parameter, x, y, yerr []float64) (Output, error)
+	XErrorsAndScatter(p0 []Parameter, x, y, xerr []float64) (Output, error)
+ 	XYErrorsAndScatter(p0 []Parameter, x, y, xerr, yerr []float64) (Output, error)
+}
+
+var (
+	LinePDF ModelPDF
+	PowerLawPDF ModelPDF
+)
+
+func CurvePDF(f Func) ModelPDF { return nil }
+
+type ModelPDF interface {
+	UnknownErrors(p0 []Parameter, x, y []float64) LogPDF
+
+	YErrors(p0 []Parameter, x, y, yerr []float64) LogPDF
+	XErrors(p0 []Parameter, x, y, xerr []float64) LogPDF
+ 	XYErrors(p0 []Parameter, x, y, xerr, yerr []float64) LogPDF
+
+	YErrorsAndScatter(p0 []Parameter, x, y, yerr []float64) LogPDF
+	XErrorsAndScatter(p0 []Parameter, x, y, xerr []float64) LogPDF
+ 	XYErrorsAndScatter(p0 []Parameter, x, y, xerr, yerr []float64) LogPDF
 }
 
 type Parameter struct {
@@ -37,3 +56,33 @@ func (p Parameter) UpperLimit(lim float64) Parameter { return p }
 func (p Parameter) Limits(lower, upper float64) Parameter { return p }
 func (p Parameter) LogPrior(pr func(float64) float64) Parameter { return p }
 
+type LogPDF func(param []float64) float64
+
+type Sampler struct { }
+
+type SamplerOption samplerOption
+type samplerConfig struct { }
+type samplerOption func(*samplerConfig)
+
+func NewSampler(pdf LogPDF, p0 []Parameter, opts ...SamplerOption) *Sampler {
+	return nil
+}
+
+func Walkers(n int) SamplerOption { return nil }
+func Stretch(a float64) SamplerOption { return nil }
+func Threads(n int) SamplerOption { return nil }
+func StepGranularity(n int) SamplerOption { return nil }
+
+type ConvergenceTest convergenceTest
+type convergenceTest func(chains [][][]float64) bool
+
+func (sampler *Sampler) Samples(tests ...ConvergenceTest) ([][]float64, error) {
+	return nil, nil
+}
+
+func (sampler *Sampler) RawSamples(steps int) ([][]float64, error) {
+	return nil, nil
+}
+
+func (sampler *Sampler) AcceptanceRatio() float64 { return 0.0 }
+func (sampler *Sampler) MostLikelyParameters() []float64 { return nil }
